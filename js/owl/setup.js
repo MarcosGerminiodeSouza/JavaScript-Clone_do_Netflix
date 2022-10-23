77e487312bae23ee546d7dc0c9ca948dd4917b46
MarcosGerminiodeSouza <marcos.germiniodesouza@gmail.com>
2022-10-23 11:21:03 -0300

    netflix-clone_tema_arcane

---
+++ js/owl/setup.js
@@ -0,0 +1,22 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    responsive: {
+        150: {
+            items: 1
+        },
+        400: {
+            items: 2
+        },
+        600: {
+            items: 3
+        },
+        900: {
+            items: 4
+        },
+        1150: {
+            items: 5
+        }
+    }
+})
